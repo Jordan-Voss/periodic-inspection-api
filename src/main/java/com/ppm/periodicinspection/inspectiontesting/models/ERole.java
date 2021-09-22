4945f086f7d6fdde107cd44cb9ad1849355454db
Jordan-Voss <jordan.voss2@mail.dcu.ie>
2021-09-22 16:04:01 +0100

    add user auth

---
+++ src/main/java/com/ppm/periodicinspection/inspectiontesting/models/ERole.java
@@ -0,0 +1,7 @@
+package com.ppm.periodicinspection.inspectiontesting.models;
+
+public enum ERole {
+    ROLE_USER,
+    ROLE_MODERATOR,
+    ROLE_ADMIN
+}
--- src/test/java/com/ppm/periodicinspection/inspectiontesting/InspectionTestingApplicationTests.java
+++ src/test/java/com/ppm/periodicinspection/inspectiontesting/InspectionTestingApplicationTests.java
@@ -1,13 +1,13 @@
-package com.ppm.periodicinspection.inspectiontesting;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class InspectionTestingApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
-}
+//package com.ppm.periodicinspection.inspectiontesting;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class InspectionTestingApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
