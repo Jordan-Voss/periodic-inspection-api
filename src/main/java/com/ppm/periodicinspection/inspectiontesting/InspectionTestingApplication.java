a669594073038e156c7581a5c8a105c49dadf136
Jordan-Voss <jordan.voss2@mail.dcu.ie>
2021-09-22 15:33:56 +0100

    initial commit

---
+++ src/main/java/com/ppm/periodicinspection/inspectiontesting/InspectionTestingApplication.java
@@ -0,0 +1,13 @@
+package com.ppm.periodicinspection.inspectiontesting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InspectionTestingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InspectionTestingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ppm/periodicinspection/inspectiontesting/InspectionTestingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ppm.periodicinspection.inspectiontesting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InspectionTestingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
